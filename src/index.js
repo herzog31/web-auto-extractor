b672ade86ff2539d27918c5e112ff066218e9099
Mark J. Becker <herzog31@users.noreply.github.com>
2025-04-07 17:36:10 +0200

    Remove cheerio dependency

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import * as cheerio from 'cheerio';
-
 import MetaTagsParser from './parsers/metatag-parser.js';
 import MicroRdfaParser from './parsers/micro-rdfa-parser.js';
 import JsonldParser from './parsers/jsonld-parser.js';
 
 export default () => {
-  function parse(html, options) {
-    const $ = cheerio.load(html, options);
-
+  function parse(html) {
     return {
-      metatags: MetaTagsParser($),
+      metatags: MetaTagsParser(html),
       microdata: MicroRdfaParser(html, 'micro'),
       rdfa: MicroRdfaParser(html, 'rdfa'),
       jsonld: JsonldParser(html),
